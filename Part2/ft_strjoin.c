120360f0b1f3c787c374f6f09018005f0a77c40c
Sejong Ko <seko@c1r6s7.42seoul.kr>
2020-10-26 21:51:29 +0900

    fix strjoin

--- Part2/ft_strjoin.c
+++ Part2/ft_strjoin.c
@@ -1,15 +1,46 @@
+#include "libft.h"
+
+char *ft_strcat(char const *s1, char const *s2)
+{
+	size_t i;
+	size_t j;
+	char *p;
+
+	i = 0;
+	j = 0;
+	p = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		p[i] = s2[j];
+		i++;
+		j++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
 char *ft_strjoin(char const *s1, char const *s2)
 {
 	size_t i;
 	char *temp;
-
-	if (!(char *)malloc(sizeof(char *) * (ft_strlen(s1) + ft_strlen(s2) + 1)))
+	char *p_p;
+	i = 0;
+	if (s1[i] == '\0')
+		return ((char *)s2);
+	if (s2[i] == '\0')
+		return ((char *)s1);
+	if (!(temp = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (0);
-	ft_strcat(s1, s2);
-	while (s1[i])
+	p_p = ft_strcat(s1, s2);
+	/*while (p_p[i])
 	{
-		temp[i] = s1[i];
+		temp[i] = p_p[i];
 		i++;
-	}
-	return (temp);
+	}*/
+	return (p_p);
 }
--- Part2/ft_strtrim.c
+++ Part2/ft_strtrim.c
@@ -1,3 +1,79 @@
-char	*ft_strtrim(char const *s1, char const *set)
-{
+#include "libft.h"
 
+int check_front(char const *s1, char const *set)
+{
+        size_t i;
+        size_t j;
+
+        i = 0;
+        j = 0;
+        while (s1[i])
+		{
+                while (set[j] != '\0')
+                {
+                        if (s1[i] == set[j])
+						   	break;
+						j++;
+                }
+				if (j == '\0')
+					return (i);
+				i++;
+				j = 0;
+		}
+		return (i);
+}
+
+int check_end(char const * s1, char const *set)
+{
+        size_t i;
+        size_t j;
+
+        i = ft_strlen(s1);//12
+        j = 0;
+        while (i > 0)
+		{
+                while (set[j] != '\0')
+                {
+                        if (s1[i] == set[j])
+                                break;
+                        j++;
+                }
+				if (j == '\0')
+					return (i);
+				i--;
+				j = 0;
+		}
+		return (i);
+}
+
+char    *ft_strtrim(char const *s1, char const *set)
+{
+        char *temp;
+        size_t front;
+        size_t end;
+        size_t i;
+		size_t j;
+
+        i = 0;
+        front = check_front(s1, set); //4
+        end = check_end(s1, set);//12
+        if (!(temp = (char *)malloc(sizeof(char *) * (end - front + 1))))
+                return (0);
+		j = front; //4
+        while (i < end + 1)
+        {
+                temp[i] = s1[j];
+                i++;
+				j++;
+        }
+		temp[i] = '\0';
+        return (temp);
+}
+
+#include <stdio.h>
+int main(void)
+{
+	char arr[50] = "Hello World!";
+	char Arr[50] = "Hlde";
+	printf("%s", ft_strtrim(arr, Arr));
+}
