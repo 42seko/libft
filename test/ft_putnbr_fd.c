1df3ad5b1bc67639ed64388336a1d8b9c4305ed6
Sejong Ko <seko@c4r7s4.42seoul.kr>
2020-11-06 18:31:55 +0900

    fix putnbr

--- test/ft_putnbr_fd.c
+++ test/ft_putnbr_fd.c
@@ -6,20 +6,34 @@
 /*   By: seko <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 20:15:40 by seko              #+#    #+#             */
-/*   Updated: 2020/11/04 20:15:48 by seko             ###   ########.fr       */
+/*   Updated: 2020/11/06 18:30:52 by seko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	ft_putchar(char c)
+{
+	write(fd, &c, 1);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	char num;
+	long int	n;
 
-	while ((n / 10) >= 10)
+	n = nb;
+	if (n < 0)
 	{
-		num = n % 10 + '0';
-		n = n / 10;
-		write(fd, "num", 1);
+		n *= -1;
+		ft_putchar('-');
+	}
+	if (n < 10)
+	{
+		ft_putchar('0' + n);
+	}
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putchar('0' + (n % 10));
 	}
 }
