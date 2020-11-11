1094e8a67f20c6b5eeaf4c7c95cb265b42f62a56
Sejong Ko <seko@c6r8s3.42seoul.kr>
2020-11-12 01:41:51 +0900

    uni test

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: seko <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 19:49:48 by seko              #+#    #+#             */
-/*   Updated: 2020/11/04 19:50:12 by seko             ###   ########.fr       */
+/*   Updated: 2020/11/12 00:18:17 by seko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 int	ft_atoi(const char *str)
 {
 	long long int	num;
-	int		sign;
-	char		*p;
+	int				sign;
+	char			*p;
 
 	num = 0;
 	sign = 1;
 	p = (char *)str;
 	while (*p == ' ' || (*p >= 9 && *p <= 13))
 		p++;
-	if ((*p == '-') || (*p == '+'))
-		if ((*(p + 1) == '-') || (*(p + 1) == '+'))
-			return (0);
 	if (*p == '-')
-		`sign *= -1;
+	{
+		sign *= -1;
+		p++;
+	}
 	while (*p >= '0' && *p <= '9')
 	{
 		if (num > 2147483647 && sign == 1)
@@ -38,10 +38,4 @@ int	ft_atoi(const char *str)
 		p++;
 	}
 	return (num * sign);
-}
-
-int main(void)
-{
-	char n[40] = "-99999999999999999999999999";
-	printf("%d", ft_atoi(n));
 }
--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: seko <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 19:39:23 by seko              #+#    #+#             */
-/*   Updated: 2020/11/04 19:39:58 by seko             ###   ########.fr       */
+/*   Updated: 2020/11/12 00:04:02 by seko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void		*ft_memcpy(void *destination, const void *source, size_t num)
 	size_t	i;
 
 	i = 0;
-	if (!dest && !src)
+	if (!destination && !source)
 		return (0);
 	while (i < num)
 	{
