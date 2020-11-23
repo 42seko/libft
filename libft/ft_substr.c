224d8032c59c29f3198a4a7e16ad5ed8e312333e
Sejong Ko <seko@c6r8s3.42seoul.kr>
2020-11-23 19:25:37 +0900

    3 try

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: seko <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 19:54:37 by seko              #+#    #+#             */
-/*   Updated: 2020/11/18 16:38:29 by seko             ###   ########.fr       */
+/*   Updated: 2020/11/23 19:17:36 by seko             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*temp;
 	size_t	length;
 
+	if (!s)
+		return (0);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
 	length = ft_strlen(s + start);
 	if (length < len)
 		len = length;
 	if (!(temp = (char *)malloc(sizeof(char) * (len + 1))))
 		return (0);
-	if (!s)
-		return (0);
-	if (ft_strlen(s) < start)
-		return (ft_strdup(""));
 	ft_strlcpy(temp, ((char *)s + start), len + 1);
 	return (temp);
 }
